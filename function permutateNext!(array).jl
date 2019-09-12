4c455491556e204f151f2ceb9fafbbc53c6a6b0e
Tim-Gaede <TimGaede@gmail.com>
2019-09-12 13:06:11 -0400

    Update function permutateNext!(array).jl

--- function permutateNext!(array).jl
+++ function permutateNext!(array).jl
@@ -1,57 +1,46 @@
-function permutateNext!(a)
-# Returns a value of true if the permutation was last
-
-    left = 1 # left index
+#-------------------------------------------------------------------------------
+function permuNext!(a)
+ # Returns a value of true if the permutation is lexicographically last.
     len = length(a)
+    iₗ = 1
 
-    # Find left index ....................
-    adj_items_out_of_order = false
+    # Find iₗ
+    disorder_fnd = false
     for i = 1 : len-1
         if a[i] < a[i+1]
-            adj_items_out_of_order = true
-            left = i
+            disorder_fnd = true
+            iₗ = i
         end
     end
-    # ...................................
 
 
-    if adj_items_out_of_order
-        # Find right index if adjacent elements were out of order
-        right = left + 1
-        for i = left + 2 : len
-            if a[left] < a[i]
-                right = i
-            end
+    if disorder_fnd
+        # Find iᵣ
+        iᵣ = iₗ + 1
+        for i = iₗ + 2 : len
+            if a[iₗ] < a[i];    iᵣ = i; end
         end
 
-        #Swap(ref a, left, right)
-        a[left], a[right]  =  a[right], a[left]
+        a[iₗ], a[iᵣ]  =  a[iᵣ], a[iₗ] # Swap
 
-        # Reverse pairs of elements to the right of left
-        pairs = (len - left) ÷ 2
+        # Swap pairs of elements to the right of iₗ
+        pairs = (len - iₗ)  ÷  2
         for p = 1 : pairs
-            a[left + p], a[len - p + 1]  =  a[len - p + 1], a[left + p]
+            a[iₗ + p], a[len - p + 1]  =  a[len - p + 1], a[iₗ + p]
         end
-
     end
 
-    !adj_items_out_of_order
+    !disorder_fnd
 end
+#-------------------------------------------------------------------------------
+
 
 
 function main()
-
-    arr = [1,2,3,4]
-
-    last = false
-    cnt = 0
-    while !last
-        println(arr)
-        last = permutateNext!(arr)
-        cnt += 1
-    end
-
-    println("\n$cnt permutations")
+    println("\n"^6)
+    arr = ['A', 'B', 'C', 'D', 'E']
+    cnt = 1
+    println(arr, " ", lpad(cnt,3))
+    while !permuNext!(arr);    cnt+=1;    println(arr, " ", lpad(cnt,3)); end
 end
 main()
-
